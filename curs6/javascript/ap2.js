766f2ae7f2b1cacd486d176820160ce3b3dc5a75
Frontend JavaScript <frontendjavascript@iMAC-06.local>
2025-02-26 20:43:15 +0200

    first commit

---
+++ curs6/javascript/ap2.js
@@ -0,0 +1,7 @@
+import Carte, {citeste} from './book2.js'
+
+
+
+let carte = new Carte('Poezii', 'Mihai Eminescu',250);
+console.log(carte.toString());
+console.log(citeste());
---
+++ curs6/javascript/ap3.js
@@ -0,0 +1,5 @@
+let adunare = (a,b) => {
+    return a + b;
+}
+
+console.log("suma dintre 24 si 46 este ",adunare(24,46));
